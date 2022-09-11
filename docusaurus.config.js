d8a655410c64ed0321aabd0b8ed6ad6016fa8513
Harpreet Singh <hsit18@gmail.com>
2022-09-12 00:20:03 +0530

    url fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Dev Docs',
   tagline: 'Devloper guide',
-  url: 'https://hsit18.github.io/dev-docs/',
-  baseUrl: '/',
+  url: 'https://hsit18.github.io',
+  baseUrl: '/dev-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
